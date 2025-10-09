34cbcea490c24826ed50373ed87a25ef3ae898d1
Justin Ware <wareisjustin@gmail.com>
2025-10-09 22:30:20 +1000

    feat: Base system layout

--- src/client/modules/shell/components/baseView.tsx
+++ src/client/modules/shell/components/baseView.tsx
@@ -1,14 +1,23 @@
 import type { NumeralSystem } from '@shared/types/base';
 import style from './baseView.module.css';
+import type { CSSProperties } from 'react';
 
-const { baseViewRoot } = style;
+const { baseViewRoot, content } = style;
 
-export const BaseView = ({ type, label }: NumeralSystem) => {
+type BaseViewPros = NumeralSystem & { position: 1 | 2 | 3 };
+
+const positionMap = new Map<number, CSSProperties>([
+  [1, { left: 0 }],
+  [2, { left: '50%', transform: 'translateX(-50%)' }],
+  [3, { right: 0 }],
+]);
+
+export const BaseView = ({ position, type, label }: BaseViewPros) => {
   const id = `${type}-view`;
 
   return (
-    <div id={id} className={baseViewRoot}>
-      {label}
+    <div id={id} className={baseViewRoot} style={positionMap.get(position)}>
+      <div className={content}>{label}</div>
     </div>
   );
 };
--- src/client/modules/shell/components/shell.tsx
+++ src/client/modules/shell/components/shell.tsx
@@ -1,6 +1,10 @@
+import base10 from '@shared/systems/base10';
 import { BaseView } from './baseView';
 import { Navigation } from './navigation';
 import styles from './shell.module.css';
+
+import base2 from '@shared/systems/base2';
+import base16 from '@shared/systems/base16';
 
 const { shellRoot, mainLayout } = styles;
 
@@ -8,7 +12,11 @@ export const Shell = () => {
   return (
     <div id="shell" className={shellRoot}>
       <Navigation />
-      <div className={mainLayout}>{/* <BaseView /> */}</div>
+      <div className={mainLayout}>
+        <BaseView {...base2} position={1} />
+        <BaseView {...base10} position={2} />
+        <BaseView {...base16} position={3} />
+      </div>
     </div>
   );
 };
